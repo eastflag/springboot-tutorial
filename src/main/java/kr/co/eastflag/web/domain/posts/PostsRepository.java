dea45c6bf32fe7f2f5035994ca5b6f9ef0e7b57e
eastflag <dklee98@>
2020-02-24 15:40:10 +0900

    JPA 설정 및 단위 테스트

---
+++ src/main/java/kr/co/eastflag/web/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package kr.co.eastflag.web.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
---
+++ src/test/java/kr/co/eastflag/domain/posts/PostsTest.java
@@ -0,0 +1,39 @@
+package kr.co.eastflag.domain.posts;
+
+import kr.co.eastflag.web.domain.posts.Posts;
+import kr.co.eastflag.web.domain.posts.PostsRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @Test
+    public void save() {
+        String title = "테스트 게시물";
+        String content ="게시판 본문";
+        String author = "eastflag";
+
+        Posts posts = Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+        postsRepository.save(posts);
+
+        List<Posts> postsList = postsRepository.findAll();
+        Posts posts1 = postsList.get(0);
+        assertThat(posts1.getTitle()).isEqualTo(title);
+        assertThat(posts1.getContent()).isEqualTo(content);
+    }
+}
