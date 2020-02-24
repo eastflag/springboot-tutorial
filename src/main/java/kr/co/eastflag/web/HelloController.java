361334d60f6d784770eb300aab86c77771c00ac5
eastflag <dklee98@>
2020-02-24 12:56:05 +0900

    HelloController 단위테스트

--- src/main/java/kr/co/eastflag/web/HelloController.java
+++ src/main/java/kr/co/eastflag/web/HelloController.java
@@ -1,6 +1,8 @@
 package kr.co.eastflag.web;
 
+import kr.co.eastflag.web.dto.HelloDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class HelloController {
     @GetMapping("hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloDto helloDto(@RequestParam("name") String name, @RequestParam("amount") Integer amount) {
+        return new HelloDto(name, amount);
     }
 }
