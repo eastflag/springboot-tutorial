0ce4b4f788fb53a569a7ad95f4a42ac55039fd75
eastflag <dklee98@>
2020-02-25 19:20:06 +0900

    구글 연동 단위테스트

---
+++ src/main/java/kr/co/eastflag/web/domain/user/Role.java
@@ -0,0 +1,15 @@
+package kr.co.eastflag.web.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "손님");
+    USER("ROLE_USER", "user");
+
+    private final String key;
+    private final String title;
+}
---
+++ src/main/java/kr/co/eastflag/web/domain/user/User.java
@@ -0,0 +1,28 @@
+package kr.co.eastflag.web.domain.user;
+
+import kr.co.eastflag.web.domain.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated
+    private Role role;
+}
