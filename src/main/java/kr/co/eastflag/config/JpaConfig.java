dda390ea26005fb3d6b6014b8b0a2310ed4c4c2c
eastflag <dklee98@>
2020-03-04 08:13:54 +0900

    spring security mvc mockup 테스트

---
+++ src/main/java/kr/co/eastflag/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package kr.co.eastflag.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
